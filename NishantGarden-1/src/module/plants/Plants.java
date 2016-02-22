9f4e4ce3d44bf919bbb5033b49b2938f3711d2ac
Nishant Phatangare <nishant1914@gmail.com>
2016-02-21 22:51:19 -0800

    Plants class updated

--- NishantGarden-1/src/module/plants/Plants.java
+++ NishantGarden-1/src/module/plants/Plants.java
@@ -13,7 +13,8 @@
 package module.plants;
 
 public abstract class Plants {
-		
+	
+	//--Variables--//
 	private double growth; 
 	// Value of growth defines how much has plant grown.
 	// growth = 0 means its a seed, growth = 1 means its fully grown plant
@@ -24,9 +25,9 @@ public abstract class Plants {
 	private double maxArea;
 	// Each plant will take some space in garden. This variable defines maximum space that this plant will take.
 	
-	//Constructors
-	protected Plants()
-	{
+	
+	//--Constructors--//
+	protected Plants() {
 		this.growth = 0.0;
 		this.growthRate = 0.01;
 		//Default plant in a garden takes 100 days to grow from seed.
@@ -34,7 +35,35 @@ public abstract class Plants {
 		//Default plant in a garden takes 0.25 sq.m area (i.e. 50 cm by 50 cm)
 	}
 	
-	//Getters
 	
+	//--Getters and Setters--//
+	public double getGrowth() {
+		return this.growth;
+	}
+	public void setGrowth(double value) {
+		this.growth = value;
+	}
+	
+	public double getGrowthRate() {
+		return this.growthRate;
+	}
+	public void setGrowthRate(double value) {
+		this.growthRate = value;
+	}
+	
+	public double getMaxArea() {
+		return this.maxArea;
+	}
+	public void setMaxArea(double value) {
+		this.maxArea = value;
+	}
+	
+	
+	//--Public Methods (Abstraction)--//
+	//Length of the strip that this plant will take
+	public double getMaxLength() {
+		return Math.sqrt(this.maxArea);
+		//We assume every plant takes area like a square shaped tile
+	}
 
 }
